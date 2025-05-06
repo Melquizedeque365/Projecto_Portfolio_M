36cf535da86c39e5173d9a4be51f274e796752ca
Melquizedeque365 <melquisedequemanuel356@gmail.com>
2025-05-06 14:01:25 +0200

    Add files via upload

---
+++ portfolio/dist/javascript/javascript.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector(".mobile-icon");
+var navBar = document.querySelector(".mobile-navbar");
+
+console.log(navBar)
+
+btn.addEventListener("click", e=>{
+
+        navBar.classList.toggle("show")
+ 
+})
